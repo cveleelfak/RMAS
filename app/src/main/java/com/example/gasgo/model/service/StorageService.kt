a3f97e2e3d692851d8b61ae92ca81e80c44f037c
cveleelfak <nikolacvele83@gmail.com>
2024-08-17 19:34:15 +0200

    dodat login screen i ostale stvari

--- app/src/main/java/com/example/gasgo/model/service/StorageService.kt
+++ app/src/main/java/com/example/gasgo/model/service/StorageService.kt
@@ -22,12 +22,12 @@ class StorageService(
         }
     }
 
-    suspend fun uploadBeachMainImage(
+    suspend fun uploadGasStationMainImage(
         image: Uri
     ): String{
         return try{
             val fileName = "${System.currentTimeMillis()}.jpg"
-            val storageRef = storage.reference.child("beaches_images/profile_images/$fileName")
+            val storageRef = storage.reference.child("GasStation_images/profile_images/$fileName")
             val uploadTask = storageRef.putFile(image).await()
             val downloadUrl = uploadTask.storage.downloadUrl.await()
             downloadUrl.toString()
@@ -37,14 +37,14 @@ class StorageService(
         }
     }
 
-    suspend fun uploadBeachGalleryImages(
+    suspend fun uploadGasStationGalleryImages(
         images: List<Uri>
     ): List<String>{
         val downloadUrls = mutableListOf<String>()
         for (image in images) {
             try {
                 val fileName = "${System.currentTimeMillis()}.jpg"
-                val storageRef = storage.reference.child("beaches_images/gallery_images/$fileName")
+                val storageRef = storage.reference.child("GasStation_images/gallery_images/$fileName")
                 val uploadTask = storageRef.putFile(image).await()
                 val downloadUrl = uploadTask.storage.downloadUrl.await()
                 downloadUrls.add(downloadUrl.toString())
